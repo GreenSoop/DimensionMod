c9eb0222f76301129e60782c08c26e43f0b58ddb
GreenSoop <arseni.trusau@gmail.com>
2025-08-03 13:20:32 +0200

    Initial commit

---
+++ src/main/java/com/dimmccmod/DimensionModDataGenerator.java
@@ -0,0 +1,11 @@
+package com.dimmccmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DimensionModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
